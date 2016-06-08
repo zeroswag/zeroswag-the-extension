86b6549f6511a563d60ca0bb65c28a925cb5ae6e
freegyes <freegyes@gmail.com>
2016-06-08 22:23:00 +0200

    add basic scripts

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var word = document.getElementsByClassName("word");
 var description = document.getElementsByClassName("description");
+var dash = document.getElementsByClassName("dash");
 
-word[0].innerHTML = "Goodbye world";
-description[0].innerHTML = "longer text";
+word[0].innerHTML = "insert word";
+description[0].innerHTML = "insert description";
+
+dash[0].addEventListener('click', function () {
+  document.body.innerHTML="<img src='dash.png'>"; 
+});
