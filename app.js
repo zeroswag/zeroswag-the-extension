94b6c5a63a06f5bbf027a96e615e5b3ef8bec471
freegyes <freegyes@gmail.com>
2016-06-08 21:26:11 +0200

    add app.js and thanks

---
+++ app.js
@@ -0,0 +1,5 @@
+var word = document.getElementsByClassName("word");
+var description = document.getElementsByClassName("description");
+
+word[0].innerHTML = "Goodbye world";
+description[0].innerHTML = "longer text";
