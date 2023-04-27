eed3d35408f164d3bb1bc950921539faf2ca7e25
Adam Freisinger <freegyes@gmail.com>
2023-04-27 20:29:39 +0200

    Update to Manifest V3 and remove general management permission

--- icon.js
+++ icon.js
@@ -1,5 +1,5 @@
 // When browser action icon is clicked, open a new tab.
-chrome.browserAction.onClicked.addListener(function() {
+chrome.action.onClicked.addListener(function() {
   var url = "chrome://newtab";
   chrome.tabs.create({ url: url });
 });
